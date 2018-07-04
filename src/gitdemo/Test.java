a3a48e0db45fd3c7c0c367df96909b3e87c15e9b
Amrendra Kumar Singh <amrendraks@operative.com>
2018-07-04 13:32:14 +0530

    2nd commit

--- src/gitdemo/Test.java
+++ src/gitdemo/Test.java
@@ -5,5 +5,6 @@ import java.net.StandardSocketOptions;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("testing");
+	System.out.println("commiting changes");
 }
 }
